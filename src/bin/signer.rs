2ad644e7102d1c84057790dcd74c758b1e10a025
Sunil Jalandhra <jalandhrasunil99@gmail.com>
2024-09-25 12:10:28 +0530

    feat: load signers from config

--- src/bin/signer.rs
+++ src/bin/signer.rs
@@ -1,3 +1,4 @@
+use core::num;
 use std::sync::Arc;
 use tokio::task;
 use tss_network::config::Settings;
@@ -9,64 +10,34 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let settings = Settings::new().expect("Failed to load configuration");
 
-    let signer1_service: Arc<SignerService> = Arc::new(
-        SignerService::new(
-            &settings.manager_url,
-            &settings.manager_port,
-            &settings.rabbitmq_uri,
-            &settings.signer1_key_file,
-            &settings.threshold,
-            &settings.total_parties,
-            &settings.path
-        )
-        .await?,
-    );
+    let num_signers = settings.total_parties as usize;
+    let total_keys = settings.signer_key_files.len();
+    if num_signers != total_keys {
+        panic!("Number of signers does not match the number of keys provided");
+    }
+    let mut signer_tasks = Vec::with_capacity(num_signers);
 
-    let signer1_task = task::spawn(async move {
-        if let Err(e) = signer1_service.run().await {
-            eprintln!("SignerService error: {:?}", e);
-        }
-    });
+    for (i, key_file) in settings.signer_key_files.iter().enumerate() {
+        let signer_service: Arc<SignerService> = Arc::new(
+            SignerService::new(
+                &settings.manager_url,
+                &settings.manager_port,
+                &settings.rabbitmq_uri,
+                key_file,
+                &settings.threshold,
+                &settings.total_parties, 
+                &settings.path
+            ).await?,
+        );
 
-    let signer2_service: Arc<SignerService> = Arc::new(
-        SignerService::new(
-            &settings.manager_url,
-            &settings.manager_port,
-            &settings.rabbitmq_uri,
-            &settings.signer2_key_file,
-            &settings.threshold,
-            &settings.total_parties,
-            &settings.path
-        )
-        .await?,
-    );
+        let signer_task = task::spawn(async move {
+            if let Err(e) = signer_service.run().await {
+                eprintln!("SignerService {} error: {:?}", i + 1, e);
+            }
+        });
+        signer_tasks.push(signer_task);
+    }
 
-    let signer2_task = task::spawn(async move {
-        if let Err(e) = signer2_service.run().await {
-            eprintln!("SignerService error: {:?}", e);
-        }
-    });
-
-    let signer3_service: Arc<SignerService> = Arc::new(
-        SignerService::new(
-            &settings.manager_url,
-            &settings.manager_port,
-            &settings.rabbitmq_uri,
-            &settings.signer3_key_file,
-            &settings.threshold,
-            &settings.total_parties,
-            &settings.path
-        )
-        .await?,
-    );
-
-    let signer3_task = task::spawn(async move {
-        if let Err(e) = signer3_service.run().await {
-            eprintln!("SignerService error: {:?}", e);
-        }
-    });
-
-    let _ = tokio::join!(signer1_task, signer2_task, signer3_task);
-
+    futures::future::join_all(signer_tasks).await;
     Ok(())
 }
